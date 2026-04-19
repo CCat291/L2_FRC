db14f9439f9d9e1d1fd4da88c77ca74db0ce8134
CCat291 <caelinmonk@gmail.com>
2026-04-20 11:30:36 +1200

    Working get items loop

--- C_05_Expenses_Loop.py
+++ C_05_Expenses_Loop.py
@@ -63,8 +63,14 @@ def get_expenses(exp_type):
 
 
 # Main routine
-print("Getting Variable Costs...")
-variable_expenses = get_expenses("variable")
-num_variable = len(variable_expenses)
-print(f"You entered {num_variable} items")
-print()
+
+# print("Getting Variable Costs...")
+# variable_expenses = get_expenses("variable")
+# num_variable = len(variable_expenses)
+# print(f"You entered {num_variable} items")
+# print()
+
+print("Getting Fixed Costs...")
+fixed_expenses = get_expenses("fixed")
+num_fixed = len(fixed_expenses)
+print(f"You entered {num_fixed} items")
